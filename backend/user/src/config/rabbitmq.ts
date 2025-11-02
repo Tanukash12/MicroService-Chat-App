e4f37754ed65797a61eca75814853921701e99c4
Tanushka12 <kashyaptanushka14@gmail.com>
2025-11-02 17:29:51 +0530

    feat: add RabbitMQ connection setup and update package dependencies

---
+++ backend/user/src/config/rabbitmq.ts
@@ -0,0 +1,21 @@
+import ampq from 'amqplib';
+
+let channel: ampq.Channel;
+
+export const connectRabbitMQ = async () => {
+    try {
+        const connection = await ampq.connect({
+            protocol: "ampq",
+            hostname: process.env.Rabbitmq_Host,
+            port: 5672,
+            username: process.env.Rabbitmq_Username,
+            password: process.env.Rabbitmq_Password
+        });
+
+        channel = await connection.createChannel();
+
+        console.log('✔Connected to RabbitMQ');
+    }catch (error) {
+        console.error('Failed to connect to RabbitMQ', error);
+    } 
+};
